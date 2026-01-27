392f5886f14db8d0800ac80458d3613bdcd803b4
Sarah F. Alvi <sarah.f3458@gmail.com>
2026-01-27 11:06:41 +0500

    Add files via upload

---
+++ task3.s
@@ -0,0 +1,3 @@
+1.1: Questions 1, 2, 3, 4(c), 4(d), 5(c), 7, 9
+
+1.3: Questions 1 (a) (b) (c) (d) (e) (f), Q7 (read and understand example 7 for this), Q8 (read and understand example 9 for this), Q12, Q13, Q17, Q18 (a)
